a301b5c9d29ed523cee68aa6dfa155a31893d0f8
Serhii Panov <spanov@integrity.com.ua>
2016-08-31 12:45:28 +0300

    6. https://angular.io/docs/ts/latest/tutorial/toh-pt5.html : Add a Dashboard

--- app/app.module.ts
+++ app/app.module.ts
@@ -4,16 +4,15 @@ import { FormsModule }   from '@angular/forms';
 
 import { routing } from './app.routing';
 import { AppComponent }  from './app.component';
+import { DashboardComponent }  from './dashboard.component';
 import { HeroDetailComponent } from './hero-detail.component';
 import { HeroesComponent } from './heroes.component';
 import { HeroService }         from './hero.service';
 
 @NgModule({
   imports: [ BrowserModule, FormsModule, routing ],
-  declarations: [ AppComponent, HeroesComponent, HeroDetailComponent ],
-  providers: [
-    HeroService
-  ],
+  declarations: [ AppComponent, DashboardComponent, HeroesComponent, HeroDetailComponent ],
+  providers: [HeroService],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
