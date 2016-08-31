4011f272635c520aa02ce6b371cb392612fd2f05
Serhii Panov <spanov@integrity.com.ua>
2016-08-31 14:36:55 +0300

    7. https://angular.io/docs/ts/latest/tutorial/toh-pt6.html : Providing HTTP Services, Simulating the web API, Heroes and HTTP

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,6 +1,11 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
+import { HttpModule }    from '@angular/http';
+
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular2-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
 
 import { routing } from './app.routing';
 import { AppComponent }  from './app.component';
@@ -10,7 +15,7 @@ import { HeroesComponent } from './heroes.component';
 import { HeroService }         from './hero.service';
 
 @NgModule({
-  imports: [ BrowserModule, FormsModule, routing ],
+  imports: [ BrowserModule, FormsModule, HttpModule, InMemoryWebApiModule.forRoot(InMemoryDataService), routing ],
   declarations: [ AppComponent, DashboardComponent, HeroesComponent, HeroDetailComponent ],
   providers: [HeroService],
   bootstrap: [ AppComponent ]
